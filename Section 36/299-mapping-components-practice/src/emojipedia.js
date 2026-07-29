c482c3608a532ecbd115cd2b8433e4c7f837d24b
Jonathan <nathan.diaz062@gmail.com>
2026-07-29 15:11:53 -0500

    Section 36 - 298-299

---
+++ Section 36/299-mapping-components-practice/src/emojipedia.js
@@ -0,0 +1,45 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "😎",
+    name: "Smiling Face With Sunglasses",
+    meaning:
+      "A yellow face with a broad, closed smile wearing black sunglasses, as if a pair of classic Wayfarers. Often used to convey the slang sense of cool."
+  },
+  {
+    id: 5,
+    emoji: "🥰",
+    name: "Smiling Face With Hearts",
+    meaning:
+      "A yellow face with smiling eyes, a closed smile, rosy cheeks, and several hearts floating around its head. Often conveys warm, positive, loving feelings, as if being in love."
+  },
+  {
+    id: 6,
+    emoji: "🤯",
+    name: "Exploding Head",
+    meaning:
+      "A yellow face with an open mouth, raised eyebrows, and an exploding head. Used to convey shock, disbelief, or amazement."
+  }
+];
+export default emojipedia;

32424f52a5f85bf72e643ebdb9b0f634c8aed1a7
Jonathan <nathan.diaz062@gmail.com>
2026-08-01 10:49:12 -0500

    Section 36 - 308-310

---
+++ Section 36/310-event-handling-in-react/src/components/App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+function App() {
+
+  const [headingText, setHeadingText] = React.useState("Hello");
+  const [mouseOver, setMouseOver] = React.useState(false);
+
+  function handleClick() {
+    setHeadingText("Submitted");
+  };
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  };
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  };
+
+
+  return (
+    <div className="container">
+      <h1>{headingText}</h1>
+      <input type="text" placeholder="What's your name?" />
+      <button
+        style={{ backgroundColor: mouseOver ? "black" : "white" }}
+        onClick={handleClick}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        Submit
+      </button>
+    </div>
+  );
+}
+
+export default App;
